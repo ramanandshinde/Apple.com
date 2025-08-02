bcf083a67b441dbc79db686acacc306daa72c472
ramanandshinde <160994126+ramanandshinde@users.noreply.github.com>
2025-08-02 21:02:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 7,
-    spaceBetween: 30,
-    freeMode: true,
-    autoplay:true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-  });
-
-  var swiper1 = new Swiper1(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper1-button-next",
-      prevEl: ".swiper1-button-prev",
-    },
-  });
+document.getElementById('registerForm').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Registration Submitted!');
+});
